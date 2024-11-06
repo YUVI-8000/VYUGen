777349d955da472de6acdd8ed8e7259710feae03
YUVI-8000 <106536884+YUVI-8000@users.noreply.github.com>
2024-11-06 12:35:28 +0530

    some changes

--- app.js
+++ app.js
@@ -52,14 +52,29 @@ app.use(methodOverride("_method"));
 //     },
 // };
 
+// const sessionOption = {
+//     secret: process.env.SECRET || 'someSecretKey',
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({
+//       mongoUrl:  process.env.MONGO_URL
+//     })
+//   };
 const sessionOption = {
     secret: process.env.SECRET || 'someSecretKey',
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl:  process.env.MONGO_URL
-    })
-  };
+      mongoUrl: process.env.MONGO_URL,
+      touchAfter: 24 * 3600 // Time period in seconds
+    }),
+    cookie: {
+        httpOnly: true,       // Prevents client-side JavaScript from accessing cookies
+        secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 1 week in milliseconds
+    }
+};
+
 app.use(session(sessionOption));
 app.use(flash());
 
